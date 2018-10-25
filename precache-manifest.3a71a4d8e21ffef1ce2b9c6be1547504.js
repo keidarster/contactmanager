85aa7caaf3a4b2c1f76e3eeac743b6eec838d7a0
Michael <keidarster@gmail.com>
2018-10-25 10:30:03 +0200

    Updates

---
+++ precache-manifest.3a71a4d8e21ffef1ce2b9c6be1547504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "806f2583955023a861ad",
+    "url": "/contactmanager/static/js/main.806f2583.chunk.js"
+  },
+  {
+    "revision": "c8c17e00ed4813b5e242",
+    "url": "/contactmanager/static/js/1.c8c17e00.chunk.js"
+  },
+  {
+    "revision": "806f2583955023a861ad",
+    "url": "/contactmanager/static/css/main.395b9295.chunk.css"
+  },
+  {
+    "revision": "3f6ee5b421bff39fadb4c2d30cfe4d87",
+    "url": "/contactmanager/index.html"
+  }
+];
